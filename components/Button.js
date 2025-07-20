c2bd9356deee951c5d7cf603f3016519e18a6ed8
Afonso Inglês <afonsorodriguesingles@gmail.com>
2025-07-20 12:15:03 +0100

    Color changes + Button now supports icon

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, Image, View } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { ActivityIndicator } from 'react-native';
 
-const Button = ({ isButtonDisabled, text, onButtonClicked, isLoading }) => {
+const Button = ({ isButtonDisabled, text, onButtonClicked, isLoading, icon }) => {
   return (
     <TouchableOpacity
       style={[buttonStyles.button, isButtonDisabled && buttonStyles.disabledButton]}
@@ -13,32 +13,45 @@ const Button = ({ isButtonDisabled, text, onButtonClicked, isLoading }) => {
       {isLoading ? (
         <ActivityIndicator size="small" color="#fff" />
       ) : (
-        <Text style={[buttonStyles.buttonText, isButtonDisabled && buttonStyles.disabledButton]}>
-          {text}
-        </Text>
+        <View style={buttonStyles.content}>
+          {icon && <Image source={icon} style={buttonStyles.icon} />}
+          <Text style={[buttonStyles.buttonText, isButtonDisabled && buttonStyles.disabledButton]}>
+            {text}
+          </Text>
+        </View>
       )}
     </TouchableOpacity>
   );
 };
 const buttonStyles = StyleSheet.create({
-    button: {
-      width: '100%',
-      height: RFValue(40),
-      backgroundColor: '#53A7FF',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 11,
-      alignSelf: 'center',
-    },
-    disabledButton: {
-      color: '#A3A3A3',
-      backgroundColor: '#366ba3',
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: RFValue(18),
-      fontFamily: 'RedHatDisplay_800ExtraBold',
-    },
-  });
+  button: {
+    width: '100%',
+    height: RFValue(40),
+    backgroundColor: '#419FFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 11,
+    alignSelf: 'center',
+  },
+  disabledButton: {
+    color: '#A3A3A3',
+    backgroundColor: '#366ba3',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: RFValue(20),
+    fontFamily: 'RedHatDisplay_800ExtraBold',
+  },
+  icon: {
+    width: RFValue(25),
+    height: RFValue(25),
+    marginRight: 10,
+  },
+  content: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Button;
